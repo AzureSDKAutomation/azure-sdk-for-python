58eb561a7d7fecfcd28f2c1fe06ebfc134031d92
SDK Automation <sdkautomation@microsoft.com>
2020-05-15 18:28:12 +0000

    Generated from d573f32d6dba48a4c30de000aaf85c7abcd021ed
    Update billing.json

--- sdk/billing/azure-mgmt-billing/azure/mgmt/billing/operations/_enrollment_accounts_operations.py
+++ sdk/billing/azure-mgmt-billing/azure/mgmt/billing/operations/_enrollment_accounts_operations.py
@@ -15,14 +15,16 @@ from msrest.pipeline import ClientRawResponse
 from .. import models
 
 
-class BillingPeriodsOperations(object):
-    """BillingPeriodsOperations operations.
+class EnrollmentAccountsOperations(object):
+    """EnrollmentAccountsOperations operations.
+
+    You should not instantiate directly this class, but create a Client instance that will create it for you and attach it as attribute.
 
     :param client: Client for service requests.
     :param config: Configuration of service client.
     :param serializer: An object model serializer.
     :param deserializer: An object model deserializer.
-    :ivar api_version: Version of the API to be used with the client request. The current version is 2018-03-01-preview. Constant value: "2018-03-01-preview".
+    :ivar api_version: The version of the API to be used with the client request. The current version is 2020-05-01. Constant value: "2020-05-01".
     """
 
     models = models
@@ -32,60 +34,53 @@ class BillingPeriodsOperations(object):
         self._client = client
         self._serialize = serializer
         self._deserialize = deserializer
-        self.api_version = "2018-03-01-preview"
+        self.api_version = "2020-05-01"
 
         self.config = config
 
-    def list(
-            self, filter=None, skiptoken=None, top=None, custom_headers=None, raw=False, **operation_config):
-        """Lists the available billing periods for a subscription in reverse
-        chronological order. This is only supported for Azure Web-Direct
-        subscriptions. Other subscription types which were not purchased
-        directly through the Azure web portal are not supported through this
-        preview API.
+    def list_by_billing_account_name(
+            self, billing_account_name, expand=None, filter=None, custom_headers=None, raw=False, **operation_config):
+        """Lists the enrollment accounts for a billing account. The operation is
+        supported only for billing accounts with agreement type Enterprise
+        Agreement.
 
-        :param filter: May be used to filter billing periods by
-         billingPeriodEndDate. The filter supports 'eq', 'lt', 'gt', 'le',
-         'ge', and 'and'. It does not currently support 'ne', 'or', or 'not'.
+        :param billing_account_name: The ID that uniquely identifies a billing
+         account.
+        :type billing_account_name: str
+        :param expand: May be used to expand the department.
+        :type expand: str
+        :param filter: May be used to filter by account name. The filter
+         supports 'eq', 'lt', 'gt', 'le', 'ge', and 'and'. It does not
+         currently support 'ne', 'or', or 'not'. Tag filter is a key value pair
+         string where key and value are separated by a colon (:).
         :type filter: str
-        :param skiptoken: Skiptoken is only used if a previous operation
-         returned a partial result. If a previous response contains a nextLink
-         element, the value of the nextLink element will include a skiptoken
-         parameter that specifies a starting point to use for subsequent calls.
-        :type skiptoken: str
-        :param top: May be used to limit the number of results to the most
-         recent N billing periods.
-        :type top: int
         :param dict custom_headers: headers that will be added to the request
         :param bool raw: returns the direct response alongside the
          deserialized response
         :param operation_config: :ref:`Operation configuration
          overrides<msrest:optionsforoperations>`.
-        :return: An iterator like instance of BillingPeriod
+        :return: An iterator like instance of EnrollmentAccount
         :rtype:
-         ~azure.mgmt.billing.models.BillingPeriodPaged[~azure.mgmt.billing.models.BillingPeriod]
+         ~azure.mgmt.billing.models.EnrollmentAccountPaged[~azure.mgmt.billing.models.EnrollmentAccount]
         :raises:
          :class:`ErrorResponseException<azure.mgmt.billing.models.ErrorResponseException>`
         """
-        def internal_paging(next_link=None, raw=False):
-
+        def prepare_request(next_link=None):
             if not next_link:
                 # Construct URL
-                url = self.list.metadata['url']
+                url = self.list_by_billing_account_name.metadata['url']
                 path_format_arguments = {
-                    'subscriptionId': self._serialize.url("self.config.subscription_id", self.config.subscription_id, 'str')
+                    'billingAccountName': self._serialize.url("billing_account_name", billing_account_name, 'str')
                 }
                 url = self._client.format_url(url, **path_format_arguments)
 
                 # Construct parameters
                 query_parameters = {}
                 query_parameters['api-version'] = self._serialize.query("self.api_version", self.api_version, 'str')
+                if expand is not None:
+                    query_parameters['$expand'] = self._serialize.query("expand", expand, 'str')
                 if filter is not None:
                     query_parameters['$filter'] = self._serialize.query("filter", filter, 'str')
-                if skiptoken is not None:
-                    query_parameters['$skiptoken'] = self._serialize.query("skiptoken", skiptoken, 'str')
-                if top is not None:
-                    query_parameters['$top'] = self._serialize.query("top", top, 'int', maximum=100, minimum=1)
 
             else:
                 url = next_link
@@ -93,7 +88,7 @@ class BillingPeriodsOperations(object):
 
             # Construct headers
             header_parameters = {}
-            header_parameters['Content-Type'] = 'application/json; charset=utf-8'
+            header_parameters['Accept'] = 'application/json'
             if self.config.generate_client_request_id:
                 header_parameters['x-ms-client-request-id'] = str(uuid.uuid1())
             if custom_headers:
@@ -102,9 +97,13 @@ class BillingPeriodsOperations(object):
                 header_parameters['accept-language'] = self._serialize.header("self.config.accept_language", self.config.accept_language, 'str')
 
             # Construct and send request
-            request = self._client.get(url, query_parameters)
-            response = self._client.send(
-                request, header_parameters, stream=False, **operation_config)
+            request = self._client.get(url, query_parameters, header_parameters)
+            return request
+
+        def internal_paging(next_link=None):
+            request = prepare_request(next_link)
+
+            response = self._client.send(request, stream=False, **operation_config)
 
             if response.status_code not in [200]:
                 raise models.ErrorResponseException(self._deserialize, response)
@@ -112,32 +111,39 @@ class BillingPeriodsOperations(object):
             return response
 
         # Deserialize response
-        deserialized = models.BillingPeriodPaged(internal_paging, self._deserialize.dependencies)
-
+        header_dict = None
         if raw:
             header_dict = {}
-            client_raw_response = models.BillingPeriodPaged(internal_paging, self._deserialize.dependencies, header_dict)
-            return client_raw_response
+        deserialized = models.EnrollmentAccountPaged(internal_paging, self._deserialize.dependencies, header_dict)
 
         return deserialized
-    list.metadata = {'url': '/subscriptions/{subscriptionId}/providers/Microsoft.Billing/billingPeriods'}
+    list_by_billing_account_name.metadata = {'url': '/providers/Microsoft.Billing/billingAccounts/{billingAccountName}/enrollmentAccounts'}
 
     def get(
-            self, billing_period_name, custom_headers=None, raw=False, **operation_config):
-        """Gets a named billing period.  This is only supported for Azure
-        Web-Direct subscriptions. Other subscription types which were not
-        purchased directly through the Azure web portal are not supported
-        through this preview API.
+            self, billing_account_name, enrollment_account_name, expand=None, filter=None, custom_headers=None, raw=False, **operation_config):
+        """Gets an enrollment account by ID. The operation is supported only for
+        billing accounts with agreement type Enterprise Agreement.
 
-        :param billing_period_name: The name of a BillingPeriod resource.
-        :type billing_period_name: str
+        :param billing_account_name: The ID that uniquely identifies a billing
+         account.
+        :type billing_account_name: str
+        :param enrollment_account_name: The ID that uniquely identifies an
+         enrollment account.
+        :type enrollment_account_name: str
+        :param expand: May be used to expand the department.
+        :type expand: str
+        :param filter: May be used to filter by account name. The filter
+         supports 'eq', 'lt', 'gt', 'le', 'ge', and 'and'. It does not
+         currently support 'ne', 'or', or 'not'. Tag filter is a key value pair
+         string where key and value are separated by a colon (:).
+        :type filter: str
         :param dict custom_headers: headers that will be added to the request
         :param bool raw: returns the direct response alongside the
          deserialized response
         :param operation_config: :ref:`Operation configuration
          overrides<msrest:optionsforoperations>`.
-        :return: BillingPeriod or ClientRawResponse if raw=true
-        :rtype: ~azure.mgmt.billing.models.BillingPeriod or
+        :return: EnrollmentAccount or ClientRawResponse if raw=true
+        :rtype: ~azure.mgmt.billing.models.EnrollmentAccount or
          ~msrest.pipeline.ClientRawResponse
         :raises:
          :class:`ErrorResponseException<azure.mgmt.billing.models.ErrorResponseException>`
@@ -145,18 +151,22 @@ class BillingPeriodsOperations(object):
         # Construct URL
         url = self.get.metadata['url']
         path_format_arguments = {
-            'subscriptionId': self._serialize.url("self.config.subscription_id", self.config.subscription_id, 'str'),
-            'billingPeriodName': self._serialize.url("billing_period_name", billing_period_name, 'str')
+            'billingAccountName': self._serialize.url("billing_account_name", billing_account_name, 'str'),
+            'enrollmentAccountName': self._serialize.url("enrollment_account_name", enrollment_account_name, 'str')
         }
         url = self._client.format_url(url, **path_format_arguments)
 
         # Construct parameters
         query_parameters = {}
         query_parameters['api-version'] = self._serialize.query("self.api_version", self.api_version, 'str')
+        if expand is not None:
+            query_parameters['$expand'] = self._serialize.query("expand", expand, 'str')
+        if filter is not None:
+            query_parameters['$filter'] = self._serialize.query("filter", filter, 'str')
 
         # Construct headers
         header_parameters = {}
-        header_parameters['Content-Type'] = 'application/json; charset=utf-8'
+        header_parameters['Accept'] = 'application/json'
         if self.config.generate_client_request_id:
             header_parameters['x-ms-client-request-id'] = str(uuid.uuid1())
         if custom_headers:
@@ -165,20 +175,19 @@ class BillingPeriodsOperations(object):
             header_parameters['accept-language'] = self._serialize.header("self.config.accept_language", self.config.accept_language, 'str')
 
         # Construct and send request
-        request = self._client.get(url, query_parameters)
-        response = self._client.send(request, header_parameters, stream=False, **operation_config)
+        request = self._client.get(url, query_parameters, header_parameters)
+        response = self._client.send(request, stream=False, **operation_config)
 
         if response.status_code not in [200]:
             raise models.ErrorResponseException(self._deserialize, response)
 
         deserialized = None
-
         if response.status_code == 200:
-            deserialized = self._deserialize('BillingPeriod', response)
+            deserialized = self._deserialize('EnrollmentAccount', response)
 
         if raw:
             client_raw_response = ClientRawResponse(deserialized, response)
             return client_raw_response
 
         return deserialized
-    get.metadata = {'url': '/subscriptions/{subscriptionId}/providers/Microsoft.Billing/billingPeriods/{billingPeriodName}'}
+    get.metadata = {'url': '/providers/Microsoft.Billing/billingAccounts/{billingAccountName}/enrollmentAccounts/{enrollmentAccountName}'}
