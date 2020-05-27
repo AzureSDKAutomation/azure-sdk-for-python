caae32656736aec213b0036b859cac01f2f12377
SDK Automation <sdkautomation@microsoft.com>
2020-05-27 05:59:35 +0000

    Generated from c9db137d8fc79c22c0f7d1888714cfda4814e8ad
    Test on windowsiot for tf

--- sdk/azure-mgmt-windowsiot/azure/mgmt/windowsiot/_device_services.py
+++ sdk/azure-mgmt-windowsiot/azure/mgmt/windowsiot/_device_services.py
@@ -44,7 +44,7 @@ class DeviceServices(SDKClient):
         super(DeviceServices, self).__init__(self.config.credentials, self.config)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
-        self.api_version = '2019-06-01'
+        self.api_version = '2019-06-02'
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
 
