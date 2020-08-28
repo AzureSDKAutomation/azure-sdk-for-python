20c0eabee920cbdf6dcb02903ae70816beec00b4
SDK Automation <sdkautomation@microsoft.com>
2020-08-28 03:14:18 +0000

    Generated from 084db6baead5e591e43b4b3a771e5a149da2617d
    Update scheduledQueryRule_API.json

--- sdk/monitor/azure-mgmt-monitor/azure/mgmt/monitor/aio/_monitor_client_async.py
+++ sdk/monitor/azure-mgmt-monitor/azure/mgmt/monitor/aio/_monitor_client_async.py
@@ -128,6 +128,7 @@ class MonitorClient(MultiApiClientMixin, _SDKClient):
            * 2019-06-01: :mod:`v2019_06_01.models<azure.mgmt.eventhub.v2019_06_01.models>`
            * 2019-10-17-preview: :mod:`v2019_10_17.models<azure.mgmt.eventhub.v2019_10_17.models>`
            * 2020-01-01-preview: :mod:`v2020_01_01_preview.models<azure.mgmt.eventhub.v2020_01_01_preview.models>`
+           * 2020-05-01-preview: :mod:`v2020_05_01_preview.models<azure.mgmt.eventhub.v2020_05_01_preview.models>`
         """
         if api_version == '2015-04-01':
             from ..v2015_04_01 import models
@@ -185,6 +186,9 @@ class MonitorClient(MultiApiClientMixin, _SDKClient):
             return models
         elif api_version == '2020-01-01-preview':
             from ..v2020_01_01_preview import models
+            return models
+        elif api_version == '2020-05-01-preview':
+            from ..v2020_05_01_preview import models
             return models
         raise NotImplementedError("APIVersion {} is not available".format(api_version))
 
@@ -574,10 +578,13 @@ class MonitorClient(MultiApiClientMixin, _SDKClient):
         """Instance depends on the API version:
 
            * 2018-04-16: :class:`ScheduledQueryRulesOperations<azure.mgmt.eventhub.v2018_04_16.aio.operations_async.ScheduledQueryRulesOperations>`
+           * 2020-05-01-preview: :class:`ScheduledQueryRulesOperations<azure.mgmt.eventhub.v2020_05_01_preview.aio.operations_async.ScheduledQueryRulesOperations>`
         """
         api_version = self._get_api_version('scheduled_query_rules')
         if api_version == '2018-04-16':
             from ..v2018_04_16.aio.operations_async import ScheduledQueryRulesOperations as OperationClass
+        elif api_version == '2020-05-01-preview':
+            from ..v2020_05_01_preview.aio.operations_async import ScheduledQueryRulesOperations as OperationClass
         else:
             raise NotImplementedError("APIVersion {} is not available".format(api_version))
         return OperationClass(self._client, self._config, Serializer(self._models_dict(api_version)), Deserializer(self._models_dict(api_version)))
