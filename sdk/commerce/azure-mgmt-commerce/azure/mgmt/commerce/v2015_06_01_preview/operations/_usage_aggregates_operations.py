92a0635447d8ffdb78a5accd0271c113d5906a8a
SDK Automation <sdkautomation@microsoft.com>
2020-10-27 02:17:33 +0000

    Generated from 04bd1635dc30c9d3a0bb78ba63e8594dab0e4c88

--- sdk/commerce/azure-mgmt-commerce/azure/mgmt/commerce/v2015_06_01_preview/operations/_usage_aggregates_operations.py
+++ sdk/commerce/azure-mgmt-commerce/azure/mgmt/commerce/v2015_06_01_preview/operations/_usage_aggregates_operations.py
@@ -18,6 +18,8 @@ from .. import models
 class UsageAggregatesOperations(object):
     """UsageAggregatesOperations operations.
 
+    You should not instantiate directly this class, but create a Client instance that will create it for you and attach it as attribute.
+
     :param client: Client for service requests.
     :param config: Configuration of service client.
     :param serializer: An object model serializer.
@@ -39,6 +41,7 @@ class UsageAggregatesOperations(object):
     def list(
             self, reported_start_time, reported_end_time, show_details=None, aggregation_granularity="Daily", continuation_token=None, custom_headers=None, raw=False, **operation_config):
         """Query aggregated Azure subscription consumption data for a date range.
+        Nit.
 
         :param reported_start_time: The start of the time range to retrieve
          data for.
@@ -75,8 +78,7 @@ class UsageAggregatesOperations(object):
         :raises:
          :class:`ErrorResponseException<azure.mgmt.commerce.models.ErrorResponseException>`
         """
-        def internal_paging(next_link=None, raw=False):
-
+        def prepare_request(next_link=None):
             if not next_link:
                 # Construct URL
                 url = self.list.metadata['url']
@@ -103,7 +105,7 @@ class UsageAggregatesOperations(object):
 
             # Construct headers
             header_parameters = {}
-            header_parameters['Content-Type'] = 'application/json; charset=utf-8'
+            header_parameters['Accept'] = 'application/json'
             if self.config.generate_client_request_id:
                 header_parameters['x-ms-client-request-id'] = str(uuid.uuid1())
             if custom_headers:
@@ -112,9 +114,13 @@ class UsageAggregatesOperations(object):
                 header_parameters['accept-language'] = self._serialize.header("self.config.accept_language", self.config.accept_language, 'str')
 
             # Construct and send request
-            request = self._client.get(url, query_parameters)
-            response = self._client.send(
-                request, header_parameters, stream=False, **operation_config)
+            request = self._client.get(url, query_parameters, header_parameters)
+            return request
+
+        def internal_paging(next_link=None):
+            request = prepare_request(next_link)
+
+            response = self._client.send(request, stream=False, **operation_config)
 
             if response.status_code not in [200]:
                 raise models.ErrorResponseException(self._deserialize, response)
@@ -122,12 +128,10 @@ class UsageAggregatesOperations(object):
             return response
 
         # Deserialize response
-        deserialized = models.UsageAggregationPaged(internal_paging, self._deserialize.dependencies)
-
+        header_dict = None
         if raw:
             header_dict = {}
-            client_raw_response = models.UsageAggregationPaged(internal_paging, self._deserialize.dependencies, header_dict)
-            return client_raw_response
+        deserialized = models.UsageAggregationPaged(internal_paging, self._deserialize.dependencies, header_dict)
 
         return deserialized
     list.metadata = {'url': '/subscriptions/{subscriptionId}/providers/Microsoft.Commerce/UsageAggregates'}
