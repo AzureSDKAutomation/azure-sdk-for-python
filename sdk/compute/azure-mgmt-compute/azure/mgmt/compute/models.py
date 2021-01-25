cb23448e72aa38bc0f01002341cc6f49b75a84f8
SDKAuto <sdkautomation@microsoft.com>
2021-01-25 15:17:58 +0000

    CodeGen from PR 12615 in Azure/azure-rest-api-specs Merge 02c0f25edb93c9f166b4b12ab9cc1f9f8fc61500 into 082a554784b6f6658998b7fbcc8c030390e7d0d3

--- sdk/compute/azure-mgmt-compute/azure/mgmt/compute/models.py
+++ sdk/compute/azure-mgmt-compute/azure/mgmt/compute/models.py
@@ -6,4 +6,5 @@
 # --------------------------------------------------------------------------
 from .v2019_04_01.models import *
 from .v2020_06_01.models import *
+from .v2020_06_30.models import *
 from .v2020_09_30.models import *
